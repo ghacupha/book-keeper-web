78104c066d36515dc9e1582122c9f8894c9faf28
Edwin Njeru <mailnjeru@gmail.com>
2023-05-22 13:38:36 +0300

    Initial version of bookKeeperWeb generated by generator-jhipster@7.9.3

---
+++ src/main/java/io/github/keeper/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package io.github.keeper.repository;
+
+import io.github.keeper.domain.Authority;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Spring Data R2DBC repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
---
+++ src/main/java/io/github/keeper/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package io.github.keeper.web.rest.vm;
